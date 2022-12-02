8a3b01d13a8b6a3c7d6ceb385cc866fd09c68bcb
ahrorjabborov <ahror7j@gmail.com>
2022-12-02 16:53:43 +0300

    =mid progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,55 @@
+import 'package:belly_kitchen/providers/settings_providers.dart';
+import 'package:belly_kitchen/ui/screens/about.dart';
+import 'package:belly_kitchen/ui/screens/collection.dart';
+import 'package:belly_kitchen/ui/screens/general.dart';
+import 'package:belly_kitchen/ui/screens/onboarding.dart';
+import 'package:belly_kitchen/ui/widgets/tabs/search_tab.dart';
+import 'package:belly_kitchen/ui/screens/settings.dart';
+import 'package:dotenv/dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'constants/theme.dart';
+import 'models/meal.dart';
+
+var dotenv = DotEnv(includePlatformEnvironment: true)..load();
+final List<Meal> favMeals = [];
+Future<void> main() async {
+  await Supabase.initialize(
+      url: 'https://xhmjwwoukqigrerthyql.supabase.co',
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobWp3d291a3FpZ3JlcnRoeXFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0NzY4MDYsImV4cCI6MTk4MzA1MjgwNn0.7AG5ufCcAK1wI5Jw33KtSj_XKIlIDgMb3Ygs5SHnHWI');
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  final themeMode = ThemeMode.light;
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final settings = ref.watch(settingsProvider);
+
+    final themeMode = settings.maybeWhen(
+        data: (data) =>
+            data.themeMode == 'Light' ? ThemeMode.light : ThemeMode.dark,
+        orElse: () => ThemeMode.system);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        title: 'Flutter Demo',
+        home: const Onboarding(),
+        routes: <String, WidgetBuilder>{
+          '/home': (BuildContext context) => const Home(),
+          '/settings': (BuildContext context) => const Settings(),
+          '/search': (BuildContext context) => const SearchTab(),
+          '/about': (BuildContext context) => const About(),
+          '/collection': (BuildContext context) => const Collection(
+                data: [],
+              )
+        },
+        themeMode: themeMode,
+        theme: AppTheme.light,
+        darkTheme: AppTheme.dark);
   }
 }
